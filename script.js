752b6a859d3d4c86f416d880859a18edf0edbeb1
Thextender42 <alanc8327@gmail.com>
2026-04-17 09:08:08 -0300

    Atualizar o script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 document.getElementById("whatsappBtn").addEventListener("click", function() {
   const numero = "5519996992683"; // Substitua pelo seu número com DDD
-  const mensagem = "Olá, gostaria de contratar seus serviços!";
+  const mensagem = "Olá Dev-Lumin tudo bem ? , gostaria de contratar seus serviços!";
   const url = `https://wa.me/${numero}?text=${encodeURIComponent(mensagem)}`;
   window.open(url, "_blank");
 });
